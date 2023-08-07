cfe3aa4feeb475aad61401247e50f5e68d65d0df
tarassim <71590238+tarassim@users.noreply.github.com>
2023-08-07 20:05:25 +0200

    Add files via upload

--- PM.py
+++ PM.py
@@ -3,8 +3,9 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import requests
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
 import time
+import os
 
 # URL des HTTP-Servers der Die Daten des Fotowiederstandes liefert
 url = "http://192.168.178.156"
@@ -46,38 +47,53 @@ def mail(empfaenger):
 	finally:
 		server.quit()
 
-# Daten vom HTTP-Server abfragen & mainloop
-while True:
-	try:
-		response = requests.get(url)
-		if response.status_code == 200:
-			data = response.json()
-			if 'Wert' in data:
-				wert = data['Wert']
-				aktwert = {wert}
-		else:
-			print(f'Fehler: {response.status_code}')
-	except requests.exceptions.RequestException as e:
-		print(f'Fehler bei der Anfrage: {e}')
-	except json.JSONDecodeError as e:
-		print(f'Fehler beim Parsen der JSON-Antwort: {e}')
-		# Wenn der Schwellwert unterschritten(Licht/Display an) wurde:
-	if(wert<schwellwert):
-		print(datetime.now())
-		loginhalt = {}
-		neulog=""
-		with open('./log.json','r') as datei:
-			loginhalt = json.load(datei)
-			neulog = {
-				'zeit': str(datetime.now()),
-				'ereignis': "Fehler"
-				}
-		with open('./log.json','w') as datei:	
-			loginhalt['log'].append(neulog)
-			json.dump(loginhalt, datei)
-		mailadressen = {}
-		with open('mails.json', 'r') as datei:
-			mailadressen = json.load(datei)
-		for e in mailadressen['mail']:
-			mail(e)
+def frageAb():		
+		try:
+			response = requests.get(url)
+			if response.status_code == 200:
+				data = response.json()
+				if 'Wert' in data:
+					wert = data['Wert']
+					aktwert = {wert}
+	
+			else:
+				print(f'Fehler: {response.status_code}')
+		except requests.exceptions.RequestException as e:
+			print(f'Fehler bei der Anfrage: {e}')
+		except json.JSONDecodeError as e:
+			print(f'Fehler beim Parsen der JSON-Antwort: {e}')
+			# Wenn der Schwellwert unterschritten(Licht/Display an) wurde:
+		if(wert<schwellwert):
+			print(datetime.now())
+			loginhalt = {}
+			neulog=""
+			with open('./log.json','r') as datei:
+				loginhalt = json.load(datei)
+				neulog = {
+					'zeit': str(datetime.now()),
+					'ereignis': "Fehler"
+					}
+			with open('./log.json','w') as datei:	
+				loginhalt['log'].append(neulog)
+				json.dump(loginhalt, datei)
+			configinhalt = {"NextRun":(time.time() * 1000)+(60*60*1000)}
+			with open('./config.json','w') as datei:
+				json.dump(configinhalt,datei)
+			mailadressen = {}
+			with open('mails.json', 'r') as datei:
+				mailadressen = json.load(datei)
+			for e in mailadressen['mail']:
+				mail(e)
 
+# Daten vom HTTP-Server abfragen & Mainloop, laufe 55 Durchgänge, danach wird das Programm vom System neu gestartet
+for i in range(55):
+        # Spamschutz
+        if(not os.path.isfile('./config.json')):
+                frageAb()
+        else:
+                with open('./config.json','r') as datei2:
+                        confinhalt = json.load(datei2)
+                        if (confinhalt['NextRun']<time.time() * 1000):
+                                frageAb()
+        # Warte eine Sekunde
+        time.sleep(1) 
